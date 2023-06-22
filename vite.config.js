d100a26c7b78510f287e8b82e6ab336a77a626de
fj-vega <fvj.114@hotmail.com>
2023-06-22 19:56:16 +0200

    Deploy application

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,9 @@ import Unfonts from "unplugin-fonts/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    outDir: "docs",
+  },
   plugins: [
     react(),
     reactRefresh(),
